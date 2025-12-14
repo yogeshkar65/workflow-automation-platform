5a5339bb525744dabfd1eaa182401bbc0ca4ba61
yogeshkar65 <yogeshkar65@gmail.com>
2025-12-14 18:13:43 +0530

    Add user registration and login with JWT authentication

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 const generateToken  =require("../utils/generateToken")
 const registerUser = async (req,res) =>{
     try{
@@ -38,4 +39,32 @@ const registerUser = async (req,res) =>{
 }
 
 };
-module.exports = { registerUser };
+const loginUser = async (req,res) =>{
+    try{
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message : "Email and Password are required"});
+        }
+        const user = await User.findOne({email}).select("+password");
+        if(!user){
+            return res.status(401).json({message :"Invalid email or password"});
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(401).json({message :"Invalid email or password"});
+        }
+        res.status(200).json({
+                _id : user._id,
+                name  : user.name,
+                email : user.email,
+                role : user.role,
+                token : generateToken(user._id)
+        
+        });
+    }
+    catch(error){
+        console.error("LOGIN ERROR FULL:", error);
+        res.status(500).json({message : error.message});
+    }
+}
+module.exports = { registerUser, loginUser };
