56482bd781eda644a40788e0abf6aff4c97e3adb
yogeshkar65 <yogeshkar65@gmail.com>
2026-01-28 16:13:01 +0530

    Fix API base URL to use VITE_API_URL

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,22 +1,24 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
+
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+const token = localStorage.getItem("token");
+if (token) {
+config.headers.Authorization = `Bearer ${token}`;
+}
+return config;
 });
 
+
 api.interceptors.response.use(
-  (res) => res,
-  (error) => {
-    return Promise.reject(error);
-  }
+(res) => res,
+(error) => Promise.reject(error)
 );
+
 
 export default api;
