019d76e2655f91e3fd26bcd45428c79f3a5542c5
yogeshkar65 <yogeshkar65@gmail.com>
2026-01-30 15:13:38 +0530

    UI polish: spacing + status feedback

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,20 +1,24 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
+
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+const token = localStorage.getItem("token");
+if (token) {
+config.headers.Authorization = `Bearer ${token}`;
+}
+return config;
 });
 
+
 api.interceptors.response.use(
-  (res) => res,
-  (error) => Promise.reject(error)
+(res) => res,
+(error) => Promise.reject(error)
 );
 
-export default api;
+
+export default api;  change this
