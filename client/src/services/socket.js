200cecc02c688d7a36bae92a01d1f0ca8f91ea86
yogeshkar65 <yogeshkar65@gmail.com>
2026-02-24 16:00:35 +0530

    fix: enable socket.io fallback transport for production

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -2,8 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
-  withCredentials: true,
-  transports: ["websocket"]
+  withCredentials: true
 });
 
 export default socket;
