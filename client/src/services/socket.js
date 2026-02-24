b992971ba5e3d16aee484a5f7e9594ea2df1f64a
yogeshkar65 <yogeshkar65@gmail.com>
2026-02-24 14:23:54 +0530

    Polishing and deployed

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,42 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
   withCredentials: true,
   transports: ["websocket"]
 });
 
 export default socket;
-
-
-// import { io } from "socket.io-client";
-
-// const socket = io("http://localhost:5000", {
-//   withCredentials: true,
-//   transports: ["websocket"],
-//   autoConnect: true
-// });
-
-// socket.on("connect", () => {
-//   console.log("✅ SOCKET CONNECTED:", socket.id);
-// });
-
-// socket.on("disconnect", () => {
-//   console.log("❌ SOCKET DISCONNECTED");
-// });
-
-// socket.on("connect_error", (err) => {
-//   console.log("❌ SOCKET ERROR:", err.message);
-// });
-
-// export default socket;
-
-
-// import { io } from "socket.io-client";
-
-// const socket = io("http://localhost:5000", {
-//   withCredentials: true,
-//   transports: ["websocket"]
-// });
-
-// export default socket;
